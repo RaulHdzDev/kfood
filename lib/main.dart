13917bef5490c99cff5be4262cf54f7348bb81b8
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-04-02 00:00:25 -0600

    se conecto la pantalla de carga

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kfood_app/presentacion/loginPage/loginPage.dart';
 import 'package:kfood_app/presentacion/pantallaCarga/splash.dart';
+import 'package:kfood_app/Animation/FadeAnimation.dart';
+import 'dart:async';
+
 
 void main() => runApp(MyApp());
 
@@ -10,9 +13,122 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      body: SplashScreen(),
+      body: Splash(),
     ),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+
+class Splash extends StatefulWidget {
+  @override
+  SplashScreen createState() => SplashScreen();
+}
+
+
+
+class SplashScreen extends State<Splash> {
+ @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 4),
+      () {
+        Navigator.push(
+          this.context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+      	child: Container( 
+
+
+	        child: Column(
+          
+	          children: <Widget>[
+	            Container(
+	              child: Stack(
+	                children: <Widget>[
+
+
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                     height: MediaQuery.of(context).size.height,
+                      decoration: new BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(222, 57, 0, 1),
+                            Color.fromRGBO(255, 123, 54, 1),
+                            ],
+                          )
+                      ),
+                    ),
+
+
+                  Positioned(
+                    width: 130,
+                    bottom: 300,
+                    left: 130,
+                    top: 300,
+                    
+	                    child: FadeAnimation(.1, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/logoSplash.png'),                     
+	                        )
+	                      ),         
+	                    )
+
+                      ),
+	                  ),
+
+
+                    
+                  Positioned(
+                    width: 130,
+                    bottom: 300,
+                    left: 130,
+                    top: 470,
+                    
+	                    child: FadeAnimation(1.5, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/nombreSplash.png'),                     
+	                        )
+	                      ),         
+	                    )
+
+                      ),
+	                  ),
+
+                    
+
+	                 
+
+
+
+	                ],
+	              ),
+	            ),
+
+
+	          ],
+	        ),
+	      ),
+      )
     );
   }
 }
