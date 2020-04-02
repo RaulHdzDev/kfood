a6807e8c8da09d47c79175a9da86e75b8a1cda04
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-04-01 18:21:10 -0600

    pantalla splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:kfood_app/presentacion/loginPage/loginPage.dart';
+import 'package:kfood_app/presentacion/pantallaCarga/splash.dart';
 
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      body: HomePage(),
+      body: SplashScreen(),
     ),
       debugShowCheckedModeBanner: false,
     );
--- lib/presentacion/pantallaCarga/splash.dart
+++ lib/presentacion/pantallaCarga/splash.dart
@@ -1,27 +1,95 @@
+import 'package:kfood_app/Animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
+import 'package:kfood_app/presentacion/loginPage/recuperar_contrasena.dart';
 
- 
+import '../loginPage/loginPage.dart';
+import 'dart:async';
+
+
+
 class SplashScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      title: 'Material App',
-
-      home: Scaffold(
-
-
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-          ),
-        ),
-      ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+      	child: Container( 
 
 
+
+	        child: Column(
+          
+	          children: <Widget>[
+	            Container(
+	              child: Stack(
+	                children: <Widget>[
+
+
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                     height: MediaQuery.of(context).size.height,
+                      decoration: new BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(222, 57, 0, 1),
+                            Color.fromRGBO(255, 123, 54, 1),
+                            ],
+                          )
+                      ),
+                    ),
+
+
+                  Positioned(
+                    width: 130,
+                    bottom: 300,
+                    left: 130,
+                    top: 300,
+                    
+	                    child: FadeAnimation(.1, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/logoSplash.png'),                     
+	                        )
+	                      ),         
+	                    )
+
+                      ),
+	                  ),
+
+
+                    
+                  Positioned(
+                    width: 130,
+                    bottom: 300,
+                    left: 130,
+                    top: 470,
+                    
+	                    child: FadeAnimation(1.5, Container(
+	                      decoration: BoxDecoration(
+	                        image: DecorationImage(
+	                          image: AssetImage('assets/images/nombreSplash.png'),                     
+	                        )
+	                      ),         
+	                    )
+
+                      ),
+	                  ),
+
+                    
+
+	                 
+
+
+
+	                ],
+	              ),
+	            ),
+
+
+	          ],
+	        ),
+	      ),
+      )
     );
   }
 }
