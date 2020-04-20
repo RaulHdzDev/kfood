f94b10c1f09e4fe430d4b68cffddef0e05a7c646
Uriel <urychdz@hotmail.es>
2020-04-20 11:37:30 -0500

    Mi entorno de desarrollo diferencia entre minusculas y mayusculas

--- lib/presentacion/loginPage/registrar.dart
+++ lib/presentacion/loginPage/registrar.dart
@@ -1,4 +1,4 @@
-import 'package:kfood_app/animation/FadeAnimation.dart';
+import 'package:kfood_app/Animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
