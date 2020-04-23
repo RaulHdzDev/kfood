5d510c5aff089f6568b9226976ee509a57065f31
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-04-23 03:08:04 -0500

    Se modifico el login y se agrego pantalla de perfil

--- lib/presentacion/loginPage/loginPage.dart
+++ lib/presentacion/loginPage/loginPage.dart
@@ -1,3 +1,363 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kfood_app/presentacion/loginPage/utiles/constants.dart';
+
+import '../menuPage/foodPage/foodPage.dart';
+import '../menuPage/foodPage/widgetsFood/headerFood.dart';
+import '../menuPage/menu_principal.dart';
+import 'recuperar_contrasena.dart';
+import 'registrar.dart';
+
+
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  Login createState() => Login();
+}
+
+
+
+class Login extends State<HomePage> {
+  bool _rememberMe = false;
+
+
+
+ 
+
+
+
+@override
+  Widget build(BuildContext context) {
+     double height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+               decoration: BoxDecoration(
+                 image: DecorationImage(image: AssetImage('assets/images/backHome.png'),
+                   fit: BoxFit.cover),
+                     gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                     end: Alignment.bottomLeft,
+                    colors: [Colors.white, Colors.white]
+                       ),
+        ),
+              ),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 100.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Iniciar sesión',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'OpenSans',
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 90.0),
+                      _matricula(),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      _contrasena(),
+                      _recuperarcontra(),
+                      _recuerdame(),
+                      _iniciarsesion(),
+                     // _botonesredessociales(),
+                      _crearcuenta(),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+  Widget _matricula() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Matricula',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.black,
+              ),
+              hintText: 'Ingrese su matricula',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+
+
+
+
+
+  Widget _contrasena() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Contraseña',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+              hintText: 'Ingrese su contraseña',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+
+
+
+
+
+
+  Widget _recuperarcontra() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: FlatButton(
+        onPressed: (){
+          Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context)=> RecuperarContrasena(),
+                          )
+
+                      );
+        },
+        padding: EdgeInsets.only(right: 0.0),
+        child: Text(
+          '¿Olvidaste tu contraseña?',
+          style: kLabelStyle,
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+  Widget _recuerdame() {
+    return Container(
+      height: 20.0,
+      child: Row(
+        children: <Widget>[
+          Theme(
+            data: ThemeData(unselectedWidgetColor: Colors.black),
+            child: Checkbox(
+              value: _rememberMe,
+              checkColor: Colors.white,
+              activeColor: Colors.black,
+              onChanged: (value) {
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+            ),
+          ),
+          Text(
+            'Recordar contraseña',
+            style: kLabelStyle,
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+
+
+
+  Widget _iniciarsesion() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: (){
+          Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context)=> MenuPrincipal(),
+                          )
+                      );
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+         color: Color.fromRGBO(248, 64, 0, 1),
+        child: Text(
+          'INICIAR SESIÓN',
+          style: TextStyle(
+        
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+  Widget _buildSocialBtn(Function onTap, AssetImage logo) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: logo,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+  Widget _crearcuenta() {
+    return GestureDetector(
+      onTap: (){
+          Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context)=> Registrar(),
+                          )
+
+                      );
+        },
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: '¿No tienes una cuenta?',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            TextSpan(
+              text: 'Registrate aquí',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }  
+}
+
+
+
+
+
+
+
+//ESTE ES EL CODIGO ANTERIOR DE LOGIN
+
+/*
 import 'package:flutter/material.dart';
 import 'package:kfood_app/presentacion/loginPage/loginLogic.dart';
 import 'package:kfood_app/presentacion/loginPage/recuperar_contrasena.dart';
@@ -197,3 +557,6 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+*/
--- lib/presentacion/menuPage/profilePage/profilePage.dart
+++ lib/presentacion/menuPage/profilePage/profilePage.dart
@@ -1,12 +1,234 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kfood_app/presentacion/loginPage/utiles/constants.dart';
+
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key key}) : super(key: key);
 
-  @override
+
+
+@override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Perfil"),
+     double height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+               decoration: BoxDecoration(
+                 image: DecorationImage(image: AssetImage('assets/images/perfilSesion.png'),
+                   fit: BoxFit.cover),
+                     gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                     end: Alignment.bottomLeft,
+                    colors: [Colors.white, Colors.white]
+                       ),
+        ),
+              ),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 90.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Perfil',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'OpenSans',
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 90.0),
+                      _matricula(),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      _correo(),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      _contrasena(),
+
+                      _cerrarsesion(),
+     
+           
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+
+
+
+
+
+
+  Widget _matricula() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Nombre',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.black,
+              ),
+              hintText: 'Su nombre es',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+  
+  Widget _correo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Correo',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.black,
+              ),
+              hintText: 'Su correo es',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+
+
+
+
+
+  Widget _contrasena() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Contraseña',
+          style: kLabelStyle,
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+              hintText: 'Ingrese su contraseña',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+ 
+ 
+
+  Widget _cerrarsesion() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: (){
+     
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+         color: Color.fromRGBO(248, 64, 0, 1),
+        child: Text(
+          'CERRAR SESIÓN',
+          style: TextStyle(
+        
+            color: Colors.white,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  
+ 
 }
+
