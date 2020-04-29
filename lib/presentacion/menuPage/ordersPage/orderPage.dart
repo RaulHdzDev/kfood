6bc4f84dadf36f57a1150f5b2cd286ff5272345c
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-04-29 18:08:35 -0500

    se agrego pantalla de carrito

--- lib/presentacion/menuPage/ordersPage/orderPage.dart
+++ lib/presentacion/menuPage/ordersPage/orderPage.dart
@@ -1,90 +1,144 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:kfood_app/presentacion/loginPage/utiles/constants.dart';
-import 'package:kfood_app/presentacion/menuPage/menu_principal.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:kfood_app/presentacion/loginPage/utiles/constants.dart';
+import 'package:kfood_app/presentacion/menuPage/ordersPage/models/orderItems.dart';
+
+class OrderPage extends StatelessWidget{
+  List<OrderItems> orderItems = [
+    OrderItems(number: "4", text: "Tacos", secondaryText: "Huevo verde", amount: "28.00"),
+    OrderItems(number: "1", text: "Hamburguesa", secondaryText: "Chicharrón", amount: "20.00"),
+     OrderItems(number: "1", text: "Tacos", secondaryText: "Chicharrón", amount: "20.00"),
+    OrderItems(number: "1", text: "Gorditas", secondaryText: "Bistec", amount: "20.00"),
+    OrderItems(number: "1", text: "Quesadillas", secondaryText: "de queso xd", amount: "20.00"),  
+ ];
 
 
-
-class OrderPage extends StatelessWidget {
-  const OrderPage({Key key}) : super(key: key);
-
-
-
-@override
+  @override
   Widget build(BuildContext context) {
-     double height = MediaQuery.of(context).size.height;
-    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-               decoration: BoxDecoration(
-                 image: DecorationImage(image: AssetImage('assets/images/perfilSesion.png'),
-                   fit: BoxFit.cover),
-                     gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                     end: Alignment.bottomLeft,
-                    colors: [Colors.white, Colors.white]
-                       ),
-        ),
-              ),
-              Container(
-                height: double.infinity,
-                child: SingleChildScrollView(
-                  physics: AlwaysScrollableScrollPhysics(),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 40.0,
-                    vertical: 90.0,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 10,
+        title: Text("Orden"),
+        backgroundColor: Color.fromRGBO(248, 64, 0, 1),
+        automaticallyImplyLeading: false,
+      ),
+
+
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16,right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                   SizedBox(height: 10,),         
+                   Text("Lista de",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),
+                  SizedBox(height: 0,),
+                  Text("comidas",style: TextStyle(fontWeight: FontWeight.normal,fontSize: 20)),
+                  SizedBox(height: 10,),
+                 SizedBox(height: 10,),  
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        'Orden',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'OpenSans',
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 90.0),
-        
-                      SizedBox(
-                        height: 20.0,
-                      ),
-              
-                      SizedBox(
-                        height: 20.0,
-                      ),
-              
-     
-           
+                      Text("Pedido",style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal),),
+                       Text("Precio",style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal),),
+ 
                     ],
                   ),
+
+
+                  ListView.builder(
+                    itemCount: orderItems.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index){
+                      return Column(
+                        children: <Widget>[
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                height: 24,
+                                width: 24,
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey.shade300),
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Center(
+                                  child: Text(orderItems[index].number,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 12),),
+                                ),
+                              ),
+ 
+                              SizedBox(width: 8,),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(orderItems[index].text,style: TextStyle(fontWeight: FontWeight.w600),),
+                                    SizedBox(height: 8,),
+                                    Text(orderItems[index].secondaryText,style: TextStyle(fontSize: 12,color: Colors.grey.shade500),),
+                                  ],
+                                ),
+                              ),
+                              Text(orderItems[index].amount,style: TextStyle(color: Colors.black,fontSize: 17,fontWeight: FontWeight.bold),),
+                            ],
+                          ),
+                          SizedBox(height: 14,),
+                          Divider(thickness: 1.3,color: Colors.grey.shade200,height: 1,),
+                          SizedBox(height: 14,),
+                        ],
+                      );
+                    },
+                  ),
+                  
+ 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("Total",style: TextStyle(fontSize: 25,fontWeight: FontWeight.normal),),
+                      Text("\$25.00",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+ 
+            
+          SafeArea(
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                height: 80,
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                child: FlatButton(
+                  onPressed: (){},
+                  child: Text("Ordenar",style: TextStyle(color: Colors.white,fontSize: 20, fontWeight: FontWeight.normal),),
+                  color: Color.fromRGBO(248, 64, 0, 1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
-              )
-            ],
+              ),
+            ),
           ),
-        ),
+
+ 
+Row(
+  children: <Widget>[
+    Text(" ")
+  ],
+)
+
+                ],
+              ),
+            ),
+          ),
+          
+
+ 
+
+        ],
       ),
     );
   }
-
-
-  
- 
 }
-
