5d9310b4ec9c2fa0fbbd6e3a9a2e8aa0350a94f2
RaulHdzDev <rahdz.0799@gmail.com>
2020-05-06 02:34:01 -0500

    obtener pedidos al iniciar

---
+++ lib/negocios/class/pedComGui.dart
@@ -0,0 +1,4 @@
+class PedComGui{
+  String id_pedidos_comidas_guisos;
+  String cantidad
+}
--- lib/negocios/obtenerPedidosComidasGuisos.dart
+++ lib/negocios/obtenerPedidosComidasGuisos.dart
@@ -1,15 +1,20 @@
+import 'dart:convert';
+
 import 'package:kfood_app/datos/requests.dart';
 import 'package:kfood_app/negocios/providers/ordenes.dart';
 
-Future<String> obtenerPedidosComidasGuisos(String idpedido) async{
+Future<List<String>> obtenerPedidosComidasGuisos(String idpedido) async{
   Map<String, String> body = {
     'id_pedido' : idpedido
   };
-  return await executeHttpRequest(urlFile: '/obtenerPedidosIncompletos.php', requestBody: body).then((value){
-    if(value != "null"){
-      print(value);
-      MismoPedido.idpedido = value;
-    }
+  return await executeHttpRequest(urlFile: '/obtenerPedComGui.php', requestBody: body).then((value){
+    // if(value != "null"){
+    //   print(value);
+    //   MismoPedido.idpedido = value;
+    // }
+    final decodedData = json.decode(value);
+    innerComidas.fromJsonList(decodedData['comida']);
+
     
   });
 }
