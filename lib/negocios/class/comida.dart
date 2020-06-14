0ec275b619eb5af0014a8ebfdd94c67e553493ee
Uriel <urychdz@hotmail.es>
2020-06-13 19:45:55 -0500

    es platillo?

--- lib/negocios/class/comida.dart
+++ lib/negocios/class/comida.dart
@@ -4,14 +4,16 @@ class Comida{
   String nombreComida;
   String precioUnitario;
   String estado;
+  bool isPlatillo;
 
-  Comida(this.idComida, this.nombreComida, this.precioUnitario, estado);
+  Comida(this.idComida, this.nombreComida, this.precioUnitario, this.estado, this.isPlatillo);
 
   Comida.fromJsonMap(Map<dynamic, dynamic> json){
     idComida = json['idComida'];
     nombreComida = json['nombreComida'];
     precioUnitario = json['precioUnitario'];
     estado = json['estado'];
+    isPlatillo = json['isPlatillo']=="1";
   }
 
 }
